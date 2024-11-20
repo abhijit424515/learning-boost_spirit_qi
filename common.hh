279bad30a342b4cc96019cce75d7e6d69ed85204
abhijit424515 <abhijit.amrendra.kumar@gmail.com>
2024-11-20 18:23:17 +0530

    phoenix added for ref

--- common.hh
+++ common.hh
@@ -1,5 +1,7 @@
 #include <boost/bind/bind.hpp>
 #include <boost/lambda/lambda.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/qi_char.hpp>
 #include <boost/spirit/include/qi_rule.hpp>
--- main.cc
+++ main.cc
@@ -3,29 +3,39 @@
 namespace parser {
 	namespace qi = boost::spirit::qi;
 	namespace ascii = boost::spirit::ascii;
+	namespace phoenix = boost::phoenix;
 
 	void print(double const& i) {
         std::cout << i << std::endl;
     }
 
 	template <typename Iterator>
-	bool parse(Iterator first, Iterator last)
+	bool parse(Iterator first, Iterator last, std::complex<double>& c)
 	{
+		using ascii::space;
 		using qi::double_;
 		using qi::phrase_parse;
-		using ascii::space;
-		using boost::bind;
-		using boost::placeholders::_1;
+		using qi::_1;
+		using phoenix::ref;
+
+		double rN = 0.0, iN = 0.0, len = 0.0;
+
+		auto g = (
+			double_[ref(rN) = _1, ref(len) += _1*_1]
+			| '(' >> double_[ref(rN) = _1, ref(len) += _1*_1] >> -(',' >> double_[ref(iN) = _1, ref(len) += _1*_1]) >> ')'
+		);
 
 		bool r = phrase_parse(
 			first,
 			last,
-			double_[&print] >> *(',' >> double_[bind(print,_1)]),
+			g,
 			space
 		);
 
 		if (first != last)
 			return false;
+		c = std::complex<double>(rN, iN);
+		std::cout << "Length: " << std::sqrt(len) << std::endl;
 		return r;
 	}
 }
@@ -36,9 +46,12 @@ int main() {
 		std::getline(std::cin, input);
 		std::string::iterator begin = input.begin(), end = input.end();
 		
-		if (!parser::parse(begin, end)) {
+		std::complex<double> c;
+		if (!parser::parse(begin, end, c)) {
 			std::cout << "Parsing failed" << std::endl;
 			return 1;
 		}
+
+		std::cout << "Real: " << c.real() << ", Imaginary: " << c.imag() << std::endl;
 	}
 }
